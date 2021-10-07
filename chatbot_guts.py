d96bb05cbc208b8d5099701885b2a4693eaf0caf
Hayley Owens <hayleyjeanowens@gmail.com>
2021-10-07 14:32:53 -0500

    Cosine similarity guts

--- chatbot_guts.py
+++ chatbot_guts.py
@@ -1,8 +1,44 @@
+import string
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from nltk.stem.porter import PorterStemmer
 
-#Preprocess user input
+def read_in_corpus():
+    f=open('chatbot.txt','r',errors = 'ignore')
+    raw=f.read()
+    raw = raw.lower()
+    #Install words while we're here
+    nltk.download('punkt') 
+    nltk.download('wordnet')
+    return raw
 
-#
+raw = read_in_corpus()
+sent_tokens = nltk.sent_tokenize(raw)
+word_tokens = nltk.word_tokenize(raw)
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+lemmer = WordNetLemmatizer()
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(punctiation)))
+
+#Cosine similarity
+def generate_response(user_input):
+    sent_tokens.append(user_response.lower())
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        return "Don't get it"
+    else:
+        return sent_tokens[idx]
+
+
